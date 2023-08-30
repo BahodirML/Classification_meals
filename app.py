86fd0700da48eba5cf5ed2c557f55a0a7a1df980
Bahodir Alayorov <bahodiralayorov@Bahodirs-MacBook-Pro.local>
2023-08-30 18:47:43 +0900

    Your commit message here

--- app.py
+++ app.py
@@ -1,2 +1,33 @@
-import streamlit as ast
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+#title
 
+st.title('Classification the meals')
+
+#uploading the picture
+
+file = st.file_uploader('Upload picture', type = ['png', 'gif', 'svg', 'jpeg'])
+if file:
+
+    st.image(file)
+    #PIL image
+    img = PILImage.create(file)
+
+    #model
+    model = load_learner('meals_model (1).pkl')
+
+
+
+    #prediction
+
+    pred, pred_id, prob = model.predict(img)
+
+    st.success(f"Prediction: {pred}")
+    st.info(f'Eccuracy: {prob[pred_id]*100:.1f}%')
+
+
+    #plotting
+
+    fig = px.bar(x=prob*100, y = model.dls.vocab)
+    st.plotly_chart(fig)
